ab10896703ac85bfdbd01f1dd0bffed2e7276ca0
Monsieur Video <monsieurvideo@gmail.com>
2009-05-02 20:10:43 +0000

    Apply patch from issue #1.

--- FlashVideo/Site/Brightcove.pm
+++ FlashVideo/Site/Brightcove.pm
@@ -21,6 +21,9 @@ sub find_video {
   $player_id ||= ($browser->content =~ /<param name=["']?playerID["']? value=["'](\d+) ?["']/)[0];
   $video_id ||= ($browser->content =~ /<param name=["']?\@?videoPlayer["']? value=["'](\d+)["']/)[0];
 
+  $player_id ||= ($browser->content =~ /flashVars.*playerID=(\d+)/)[0];
+  $video_id ||= ($browser->content =~ /flashVars.*videoID=(\d+)/)[0];
+
   # Support "viral" videos
   my $current_url = $browser->uri->as_string;
   if (!$video_id and $current_url =~ /bctid=(\d+)/) {
