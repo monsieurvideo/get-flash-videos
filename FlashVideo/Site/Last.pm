49fdac0fd7c3ea1eabe165167e622059237aafba
Monsieur Video <monsieurvideo@gmail.com>
2009-06-21 21:23:45 +0000

    Last.fm support (closes issue 15).

---
+++ FlashVideo/Site/Last.pm
@@ -0,0 +1,27 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::Site::Last;
+
+use strict;
+use FlashVideo::Utils;
+use URI::Escape;
+
+sub find_video {
+  my ($self, $browser, $embed_url) = @_;
+
+  my($artist, $id) = $embed_url =~ m{/([^/]+)/\+videos/(\d+)};
+  my($title) = $browser->content =~ /<h1>([^<]+)/;
+
+  die "No video ID found" unless $id;
+
+  $browser->get("http://ext.last.fm/1.0/video/getplaylist.php?&vid=$id&artist=$artist");
+
+  return $browser->content =~ /<location>([^<]+)/, title_to_filename($title);
+}
+
+sub can_handle {
+  my($self, $browser, $url) = @_;
+
+  return $url =~ /last\.fm/ && $url =~ /\+video/;
+}
+
+1;
