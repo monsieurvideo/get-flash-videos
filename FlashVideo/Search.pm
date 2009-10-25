a7c5195fd1e96cdb40d778f53a82d49d828d88d7
Zak Flash <zakflashvideo@gmail.com>
2009-10-25 21:12:16 +0000

    Added support for site and plugin search.

---
+++ FlashVideo/Search.pm
@@ -0,0 +1,110 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::Search;
+
+use strict;
+use Carp;
+
+use FlashVideo::GoogleVideoSearch;
+use FlashVideo::Utils;
+
+# Sites which support searching
+my @sites_with_search = ();
+
+sub search {
+  my ($search, $max_per_site, $max_results) = @_;
+
+  my @search_sites = map { /^FlashVideo::Site::/ ? $_ 
+                                                 : "FlashVideo::Site::$_"; }
+                     @sites_with_search;
+
+  # If a user searches for "foo something", check to see if "foo" is a site
+  # we support. If it is, only search that site.
+  if ($search =~ /^(\w+) \w+/) {
+    my $possible_site = ucfirst lc $1;
+
+    debug "Checking to see if '$possible_site' in '$search' is a search-supported site.";
+
+    my $possible_package = "FlashVideo::Site::$possible_site";
+
+    eval "require $possible_package";
+
+    if (UNIVERSAL::can($possible_package, "search")) {
+      # Only search this site
+      debug "Search for '$search' will only search $possible_site.";
+
+      # Remove the site name from the search string
+      $search =~ s/^\w+ //;
+
+      my @results = $possible_package->search($search);
+
+      trim_resultset(\@results, $max_results);
+
+      return @results;
+    }
+  }
+
+  # Check to see if any plugins have a search function defined.
+  my @plugins = main::get_installed_plugins();
+
+  foreach my $plugin (@plugins) {
+    $plugin =~ s/\.pm$//;
+
+    my $plugin_package = "FlashVideo::Site::$plugin";
+
+    eval "require $plugin_package";
+
+    if (UNIVERSAL::can($plugin_package, "search")) {
+      debug "Plugin '$plugin' has a search method.";
+
+      push @search_sites, $plugin_package;
+    }
+    else {
+      debug "Plugin '$plugin' doesn't have a search method.";
+    }
+  }
+
+  # Call each site's search method - this includes plugins and sites
+  # defined in @sites_with_search.
+  my @results;
+
+  foreach my $search_site (@search_sites) {
+    debug "Searching '$search_site' for '$search'.";
+
+    if (my @site_results = $search_site->search($search)) {
+      debug "Found " . @site_results . " results for $search.";
+
+      trim_resultset(\@site_results, $max_per_site);
+
+      push @results, @site_results;
+    }
+    else {
+      debug "No results found for '$search'.";
+    }
+  }
+
+  # Search Google as well - handle that separately for now.
+  my @google_results = FlashVideo::GoogleVideoSearch::search($search);
+
+  trim_resultset(\@google_results, $max_per_site);
+  
+  push @results, @google_results;
+
+  # Return all results, trimming if necessary.
+  trim_resultset(\@results, $max_results);
+
+  return @results;
+}
+
+sub trim_resultset {
+  my ($results, $max) = @_;
+
+  croak "Must be supplied a reference to resultset" unless ref($results) eq 'ARRAY';
+  croak "No max supplied" unless $max;
+
+  if (@$results > $max) {
+    debug "Found " . @$results . " results, trimming to $max.";
+    splice @$results, $max;
+  }
+}
+
+1;
