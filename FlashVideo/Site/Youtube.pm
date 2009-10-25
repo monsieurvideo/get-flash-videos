b612d22391a50bcaa7a2671858ee93d6a49746af
Monsieur Video <monsieurvideo@gmail.com>
2009-10-25 20:46:30 +0000

    Fix issue 40, YouTube login now works.
    Based on patch from James in the ticket, thanks.

--- FlashVideo/Site/Youtube.pm
+++ FlashVideo/Site/Youtube.pm
@@ -3,29 +3,32 @@ package FlashVideo::Site::Youtube;
 
 use strict;
 use Encode;
+use HTML::Entities;
 use FlashVideo::Utils;
 
 sub find_video {
   my ($self, $browser, $embed_url) = @_;
 
-  if($embed_url !~ m!/watch!) {
+  if($embed_url !~ m!youtube\.com/watch!) {
     $browser->get($embed_url);
-    if ($browser->response->header('Location') =~ m!/swf/.*video_id=([^&]+)!) {
-      # We ended up on a embedded SWF
-      $browser->get("http://www.youtube.com/watch?v=$1");
-    } elsif($embed_url =~ m!/v/([-a-z0-9]+)!i) {
-      $browser->get("http://www.youtube.com/watch?v=$1");
+    if ($browser->response->header('Location') =~ m!/swf/.*video_id=([^&]+)!
+        || $embed_url =~ m!/v/([-_a-z0-9]+)!i
+        || $browser->uri =~ m!v%3D([-_a-z0-9]+)!i) {
+      # We ended up on a embedded SWF or other redirect page
+      $embed_url = "http://www.youtube.com/watch?v=$1";
+      $browser->get($embed_url);
     }
   }
 
   if (!$browser->success) {
-    if ($browser->response->code == 303) {
+    if ($browser->response->code == 303 
+        && $browser->response->header('Location') =~ m!/verify_age|/accounts/!) {
       # Lame age verification page - yes, we are grown up, please just give
       # us the video!
       my $confirmation_url = $browser->response->header('Location');
       print "Unfortunately, due to Youtube being lame, you have to have\n" .
             "an account to download this video.\n" .
-            "Username: ";
+            "Username (Google Account Email): ";
       chomp(my $username = <STDIN>);
       print "Ok, need your password (will be displayed): ";
       chomp(my $password = <STDIN>);
@@ -34,26 +37,68 @@ sub find_video {
         exit 1;
       }
 
-      $browser->get("http://youtube.com/login");
-      $browser->form_name("loginForm");
-      $browser->set_fields(username => $username,
-                           password => $password);
+      $browser->get("http://www.youtube.com/login");
+      if ($browser->response->code != 303) {
+        die "Unexpected response from Youtube login.\n";
+      }
+
+      my $real_login_url = $browser->response->header('Location');
+      $browser->get($real_login_url);
+
+      $browser->form_with_fields('Email', 'Passwd');
+      $browser->set_fields(Email  => $username,
+                           Passwd => $password);
       $browser->submit();
+
       if ($browser->content =~ /your login was incorrect/) {
         error "Couldn't log you in, check your username and password.";
         exit 1;
+      } elsif ($browser->response->code == 302) {
+        # expected, next step in login process
+        my $check_cookie_url = $browser->response->header('Location');
+        $browser->get($check_cookie_url);
+
+        # and then another, html-only, non-http, redirection...
+        if ($browser->content =~ /<meta.*"refresh".*?url=&#39;(.*?)&#39;"/i) {
+          my $redirected = decode_entities($1);
+          $browser->get($redirected);
+
+          # If we weren't redirected to YouTube we might have a regional Google
+          # site.
+          if(URI->new($redirected)->host !~ /youtube/i) {
+            if($browser->response->code == 302) {
+              $browser->get($browser->response->header("Location"));
+            } else {
+              die "Did not find expected redirection";
+            }
+          }
+        } else {
+          die "Did not find expected redirection";
+        }
+      }
+      else {
+        die "Unexpected response during login";
       }
 
-      $browser->get($confirmation_url);
-      $browser->form_with_fields('next_url', 'action_confirm');
-      $browser->field('action_confirm' => 'Confirm Birth Date');
-      $browser->click_button(name => "action_confirm");
+      # Now we go back to the video page, hopefully logged in...
+      $browser->get($embed_url);
 
-      if ($browser->response->code != 303) {
-        error "Unexpected response from Youtube.";
-        exit 1;
+      # the confirmation url will always fail to show the video these days
+      # AND it'll fail to show the button apparently.
+      if ($browser->response->code == 303) {
+        # this account hasn't been enabled for grownup videos yet
+        my $real_confirmation_url = $browser->response->header('Location');
+        $browser->get($real_confirmation_url);
+        if ($browser->form_with_fields('next_url', 'action_confirm')) {
+          $browser->field('action_confirm' => 'Confirm Birth Date');
+          $browser->click_button(name => "action_confirm");
+
+          if ($browser->response->code != 303) {
+            die "Unexpected response from Youtube";
+          }
+          $browser->get($browser->response->header('Location'));
+        }
       }
-      $browser->get($browser->response->header('Location'));
     }
     else {
       # Lame Youtube redirection to uk.youtube.com and so on.
@@ -68,7 +113,8 @@ sub find_video {
   }
 
   my $video_id;
-  if ($browser->content =~ /(?:var pageVideoId =|(?:CFG_)?VIDEO_ID'?\s*:)\s*'(.+?)'/) {
+  if ($browser->content =~ /(?:var pageVideoId =|(?:CFG_)?VIDEO_ID'?\s*:)\s*'(.+?)'/
+      || $embed_url =~ /v=([^&]+)/) {
     $video_id = $1;
   } else {
     die "Couldn't extract video ID";
