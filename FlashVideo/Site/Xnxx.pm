a5f16cc1b8cd81d364f1b6a89a3041d4374abaff
Zak Flash <zakflashvideo@gmail.com>
2010-03-14 14:57:19 +0000

    Unescape Xvideos FLV URL - fixes issue 76.

--- FlashVideo/Site/Xnxx.pm
+++ FlashVideo/Site/Xnxx.pm
@@ -3,17 +3,23 @@ package FlashVideo::Site::Xnxx;
 
 use strict;
 use FlashVideo::Utils;
+use URI::Escape;
 
 sub find_video {
   my ($self, $browser, $embed_url) = @_;
 
   # Grab the file from the page..
   my $url = ($browser->content =~ /flv_url=(.+?)&/)[0];
+  $url = uri_unescape($url);
   die "Unable to extract url" unless $url;
 
   # Extract filename from page and format
   $browser->content =~ /(?:<span class="style5">|<td style="font-size: 20px;">\s*)<strong>([^<]+)/;
-  my $filename = title_to_filename($1, $url);
+
+  # title_to_filename() can't extract extension from URLs like
+  # foo.flv?stuff - should probably change, but for now don't bother
+  # passing in the URL. (Will default to .flv)
+  my $filename = title_to_filename($1);
     
   return $url, $filename;
 }
