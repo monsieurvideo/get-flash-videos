c358d729cd3d11d4e09b9235ed37ba7b84c0aff4
ZakFlash <zakflashvideo@gmail.com>
2010-04-06 17:28:28 +0100

    Fix Onion support (issue 89).

--- FlashVideo/Site/Theonion.pm
+++ FlashVideo/Site/Theonion.pm
@@ -7,51 +7,29 @@ use FlashVideo::Utils;
 sub find_video {
   my ($self, $browser) = @_;
 
-  # Get the video ID
-  my $video_id;
+  if ($browser->response->is_redirect) {
+    $browser->get( $browser->response->header('Location') );
 
-  if ($browser->content =~ /<meta name="nid" content="(\d+)"/) {
-    $video_id = $1;
-  }
-  elsif ($browser->content =~ /var videoid = ["'](\d+)["'];/) {
-    $video_id = $1;
-  }
-  
-  die "Couldn't find Onion video ID" unless $video_id;
-
-  # Get XML with FLV details
-  # TODO - support "bookend" (online exclusive videos).
-  $browser->get("http://www.theonion.com/content/xml/$video_id/video");
-
-  die "Couldn't download Onion XML: " . $browser->response->status_line
-    if !$browser->success;
-
-  # Generic can handle this XML so just pass it over to that.
-  my ($url, @filenames) = FlashVideo::Generic->find_video($browser);
-
-  # We can probably make a better guess for the filename than generic can
-  # though.
-  my $has_xml_simple = eval { require XML::Simple };
-  
-  # Don't die if XML::Simple isn't installed, as strictly speaking it's not
-  # needed for this video -- it just lets us have a better title.
-  if ($has_xml_simple) {
-    my $video_rss = eval {
-      XML::Simple::XMLin($browser->content);
-    };
-
-    die "Couldn't parse The Onion XML: $@" if $@;
-
-    if (my $title_from_rss = $video_rss->{channel}->{item}->[0]->{title}) {
-      $title_from_rss = title_to_filename($title_from_rss); 
-
-      # Likely to be a better filename so put it first for people who are
-      # using --yes
-      unshift @filenames, $title_from_rss;
+    if (!$browser->success) {
+      die "Couldn't follow Onion redirect: " .
+        $browser->response->status_line;
     }
   }
 
-  return $url, @filenames;
+  my $title;
+  if ($browser->content =~ /var video_title = "([^"]+)"/) {
+    $title = $1;
+  }
+  else {
+    $title = extract_info($browser)->{meta_title};
+  }
+
+  my $filename = title_to_filename($title);
+
+  # They now pass the URL as a param, so the generic code can extract it.
+  my $url = (FlashVideo::Generic->find_video($browser, $browser->uri))[0];
+
+  return $url, $filename;
 }
 
 1;
