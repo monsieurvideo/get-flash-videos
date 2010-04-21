e9b88fa5108b8aa81c4525119daf81685b34c376
ZakFlash <zakflashvideo@gmail.com>
2010-04-21 18:41:05 +0100

    Fixed Ted support, fixes issue 105.

--- FlashVideo/Site/Ted.pm
+++ FlashVideo/Site/Ted.pm
@@ -7,12 +7,17 @@ sub find_video {
   my ($self, $browser) = @_;
 
   my $url;
-  if($browser->content =~ m{<a href="(/talks[^"]+)">Watch this talk as high-res}) {
+  if($browser->content =~ m{<a href="(/talks[^"]+)">Watch high-res video}) {
     $url = URI->new_abs($1, $browser->uri);
     $browser->allow_redirects;
   } else {
     die "Unable to find download link";
   }
+
+  # TODO - support subtitles. Available in JSON (urgh):
+  #   http://www.ted.com/talks/subtitles/id/453/lang/eng
+  # The ID can be pulled out of flashvars:
+  #   ti:"453"
 
   my $title = extract_title($browser);
   $title =~ s/\s*\|.*//;
