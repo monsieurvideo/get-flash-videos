157c9a8f1ba5d7d66d89a2182a52f4969fdac655
Christopher Vollick <psycotica0@gmail.com>
2010-06-08 14:46:25 -0400

    Slightly Better Style.
    Oops.
    I missed the part where my editor used Tabs instead of Spaces.

--- FlashVideo/Site/Pennyarcade.pm
+++ FlashVideo/Site/Pennyarcade.pm
@@ -6,13 +6,13 @@ use FlashVideo::Utils;
 
 sub find_video {
   my ($self, $browser, $embed_url) = @_;
-	my $tempUrl = $browser->content =~ /"http:\/\/api.indieclicktv.com\/player\/show\/([a-f0-9\/]*)\/default\/mplayer.js/;
-	die "No Video Urls Found" unless $tempUrl;
-	my $url = "http://ictv-pa-ec.indieclicktv.com/media/videos/$1/video.mp4";
-	$browser->content =~ /<div class="title"><h1>([^<]*)<\/h1>/;
-	my $title = $1;
+  my $tempUrl = $browser->content =~ /"http:\/\/api.indieclicktv.com\/player\/show\/([a-f0-9\/]*)\/default\/mplayer.js/;
+  die "No Video Urls Found" unless $tempUrl;
+  my $url = "http://ictv-pa-ec.indieclicktv.com/media/videos/$1/video.mp4";
+  $browser->content =~ /<div class="title"><h1>([^<]*)<\/h1>/;
+  my $title = $1;
 
-	my $filename = "$title.mp4";
+  my $filename = "$title.mp4";
 
   return $url, $filename;
 }
