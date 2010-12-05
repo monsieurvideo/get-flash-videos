ae955711f7f1896eadda840ca68bf0fe8dde35da
Monsieur Video <monsieurvideo@gmail.com>
2010-12-05 08:51:52 +0000

    Update regexps for Tva support
    Thanks to Stavr00 in issue 202 for the patch.

--- lib/FlashVideo/Site/Tva.pm
+++ lib/FlashVideo/Site/Tva.pm
@@ -17,15 +17,15 @@ sub find_video {
   my ($self, $browser, $embed_url) = @_;
 
   # look inside script that generates CanoeVideoStandalone object
-  my $video_id  = ($browser->content =~ /CanoeVideoStandalone.SetVideo.(\d+)/i)[0];
-  my $player_id = ($browser->content =~ /CanoeVideoStandalone.SetPlayer.(\d+)/i)[0];
+  my $video_id  = ($browser->content =~ /player.SetVideo.(\d+)/i)[0];
+  my $player_id = ($browser->content =~ /player.SetPlayer.(\d+)/i)[0];
 
   debug "Extracted playerId: $player_id, videoId: $video_id"
     if $player_id or $video_id;
 
   if(!$video_id) {
     # Some pages use more complex video[x][3] type code..
-    my $video_offset = ($browser->content =~ /CanoeVideoStandalone.SetVideo.\w+\[(\d+)/i)[0];
+    my $video_offset = ($browser->content =~ /player.SetVideo.\w+\[(\d+)/i)[0];
     $video_id = ($browser->content =~ /videos\[$video_offset\].+'(\d+)'\s*\]/)[0];
   }
 
@@ -38,7 +38,7 @@ sub find_video {
 sub can_handle {
   my($self, $browser, $url) = @_;
 
-  return $browser->content =~ /CanoeVideoStandalone\.GeneratePlayer\(\);/i;
+  return $browser->content =~ /player = CanoeVideoStandalone\.create\(\);/i;
 }
 
 1;
