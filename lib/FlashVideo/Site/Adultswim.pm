99be9f99585303c90c703ce67aed47d8ad625bcd
Matthew Bauer <mjbauer95@gmail.com>
2011-05-18 23:20:44 -0500

    added preliminary support for Adultswim

---
+++ lib/FlashVideo/Site/Adultswim.pm
@@ -0,0 +1,89 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::Site::Adultswim;
+
+use strict;
+use FlashVideo::Utils;
+
+sub find_video {
+	my($self, $browser, $embed_url) = @_;
+
+	my $xml;
+	my $id;
+
+	my $segIds;
+	if($browser->{content} =~ m/(<meta[^>]* ?name=["']segIds["'] ?[^>]*>)/){
+		my $text = $1;
+		if($text =~ m/content=["']([^"']+)["']/){
+			$segIds = $1;
+		}
+	}
+
+	my $id1;
+	if($segIds =~ m/^([^#]*)#/){
+		$id1 = $1;
+	}
+
+	my $title;
+	if($browser->{content} =~ m/<meta property=["']og:title["'] content=["']([^"']+)["']\/>/){
+		$title = $1;
+	}
+
+	my $configURL = "/tools/swf/player_configs/watch_player.xml";
+
+#	foreach($xml->{head}->{script}){
+		if($browser->content =~ /pageObj\.configURL = ["']([^"']+)["'];/) {
+			$configURL = $1;
+		}
+#	}
+
+	$browser->get($configURL);
+
+	my $serviceConfigURL;
+
+	if($browser->response->code =~ /^30\d$/){
+
+		$xml = from_xml($browser);
+
+		if($xml->{serviceConfigURL} ne ""){
+			$serviceConfigURL = $1;
+		}
+	} else {
+		$serviceConfigURL = "http://asfix.adultswim.com/staged/AS.configuration.xml";
+	}
+
+	$browser->get($serviceConfigURL);
+
+	$xml = from_xml($browser);
+
+	my $getVideoPlayerURL;
+	if($xml->{config}->{services}->{getVideoPlaylist}->{url} ne ""){
+		$getVideoPlayerURL = $1;
+	} else {
+		$getVideoPlayerURL = "http://asfix.adultswim.com/asfix-svc/episodeservices/getVideoPlaylist?networkName=AS";
+	}
+
+	my $videoURL = "$getVideoPlayerURL&id=$id1";
+	$browser->get($videoURL);
+
+	$xml = from_xml($browser);
+
+#	my $pick;
+#	foreach(@{$xml->{entry}}){
+#		if(!($_->{ref}->{href} =~ m/iPhone/)){
+#			$pick = $1;
+#		}
+#	}
+
+#	grep { $_->{name} eq "mimeType" } @{$_->{param}})[0]->{value} 
+#	my $pick = (grep { $_->{param}->{value}->[3] eq "video/x-flv" } @{$xml->{entry} } )[0];
+
+	my $pick = $xml->{entry}[4];
+
+	my $file_url = $pick->{ref}->{href};
+
+	# $prefs->{quality}
+
+	return $file_url, title_to_filename($title);
+}
+
+1;
