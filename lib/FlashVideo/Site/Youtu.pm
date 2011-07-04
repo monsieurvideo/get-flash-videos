d58d28e5fc320c1262075507035e9869d185d9bf
Wolfgang Herget <wolfgang@r007.de>
2011-07-04 17:07:21 +0200

    added youtu.be alias for youtube.com

---
+++ lib/FlashVideo/Site/Youtu.pm
@@ -0,0 +1,7 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::Site::Youtu;
+
+# youtu.be is just an alias for youtube; everything else is handled further up
+use base FlashVideo::Site::Youtube;
+
+1;
