bc2f0d5a7b231028e8be175108000e8937fcde29
j <j@mailb.org>
2012-03-12 01:40:33 +0100

    cleanup Zdf.pm

--- lib/FlashVideo/Site/Zdf.pm
+++ lib/FlashVideo/Site/Zdf.pm
@@ -8,18 +8,17 @@ sub find_video {
   my ($self, $browser, $embed_url, $prefs) = @_;
   my ($id, $filename, $videourl, $quality);
 
-  debug "Zdf::find_video called, embed_url = \"$embed_url\"\n";
+  $quality = {high => 'veryhigh', low => 'low'}->{$prefs->{quality}};
 
-  my $pageurl = $browser->uri() . "";
+  debug "Zdf::find_video called, embed_url = \"$embed_url\"\n";
 
   if($browser->content =~ /\/video\/(\d*)\/(.*)"/) {
     $id = $1;
-    debug "Found video $1 $2\n";
+    debug "found video $1 $2\n";
     $filename = title_to_filename($2);
-    debug "found video \"$id\"\n";
+
     $browser->get("http://www.zdf.de/ZDFmediathek/xmlservice/web/beitragsDetails?id=$id&ak=web");
-    #qualies veryhigh high low
-    $quality = {high => 'veryhigh', low => 'low'}->{$prefs->{quality}};
+
     if($browser->content =~ /(http:\/\/fstreaming\.zdf\.de\/zdf\/$quality\/.*\.meta)/) {
         $browser->get($1);
         if($browser->content =~ /(rtmp.*)</) {
