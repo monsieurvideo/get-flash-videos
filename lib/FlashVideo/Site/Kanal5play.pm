d75dfd481013f9da6286852134e2ed65da3e2a86
Jonas <jonas.kristoffer.karlsson@gmail.com>
2012-04-26 19:51:16 +0200

    Kanal5play using Flashvideo::JSON instead

--- lib/FlashVideo/Site/Kanal5play.pm
+++ lib/FlashVideo/Site/Kanal5play.pm
@@ -1,10 +1,11 @@
 # Part of get-flash-videos. See get_flash_videos for copyright.
 package FlashVideo::Site::Kanal5play;
 
-use lib 'lib';
 use strict;
 use warnings;
 use FlashVideo::Utils;
+use FlashVideo::JSON;
+
 
 my $bitrates = {
      "low" => 250000,
@@ -19,34 +20,28 @@ sub find_video {
   my ($video_id) = $1;
   my $info_url = "http://www.kanal5play.se/api/getVideo?format=FLASH&videoId=$video_id";
   $browser->get($info_url);
+  
   if (!$browser->success){
       die "Couldn't download $info_url: " . $browser->response->status_line;
   }
-  my $name = ($browser->content =~ /"name":"(.*?)"/)[0];
-  my $episode = ($browser->content =~ /"episodeNumber":([0-9]*)/)[0];
-  my $season = ($browser->content =~ /"seasonNumber":([0-9]*)/)[0];
-  my $filename = "$name - S" . ($season < 10 ? "0" : "") . $season  . 
-                 "E" . ($episode < 10 ? "0" : "") . $episode;
-  my ($rtmp) = "rtmp://fl1.c00608.cdn.qbrick.com:1935/00608";
-  
-  my @rate_path;
-  my $count;
-  my ($temp_text) = $browser->content;
-  for ($count = 0; $count < 3; $count++){
-      $temp_text =~ /"bitrate":([0-9]*)(.*?)"source":"(.*?)"(.*)/;
-      $rate_path[$count] = { 'bitrate' => $1,
-			     'mp4'     => $3 };
-      $temp_text = $4;
-  }
-  
-  my ($playpath) = $rate_path[0]->{mp4};
-  foreach (@rate_path) {
-    my ($rate) = int($_->{bitrate});
-    if($bitrates->{$prefs->{quality}} == $rate){
-        $playpath = $_->{mp4};
-    }
-  };
 
+  my $jsonstr = $browser->content;
+  my $json = from_json($jsonstr);
+
+  my $name = $json->{program}->{name};
+  my $episode = $json->{episodeNumber};
+  my $season = $json->{seasonNumber};
+  my $filename = sprintf "%s - S%02dE%02d", $name, $season, $episode;
+  my ($rtmp) = "rtmp://fl1.c00608.cdn.qbrick.com:1935/00608";
+  my ($playpath) = $json->{streams}[0]->{source};
+
+  my  $i;
+  foreach $i (keys $json->{streams}) {
+      my ($rate) = int($json->{streams}[$i]->{bitrate});
+      if($bitrates->{$prefs->{quality}} == $rate){
+	  $playpath = $json->{streams}[$i]->{source};
+      }
+  }
   return {
       flv => title_to_filename($filename, "flv"),
       rtmp => $rtmp,
