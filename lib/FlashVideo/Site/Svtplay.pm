9902a6da6138c075f2d101ef02cd956872ad5863
ZakFlash <zakflashvideo@gmail.com>
2012-06-06 20:10:02 +0100

    Cleaned up and fixed (sort-of) SVTplay.se
    - (Style) Added blank lines where appropriate
    - (Style) Removed pointless ()
    - (Style) Removed pointless variables (e.g. $jsonstr, $srt_content)
    - (Style) () for built-in functions
    - (Style) Aligned => in hash assignment
    - (Style) Bad variable name $json (it *came* from JSON, it is not now JSON.
      It is now a data structure. Let the name convey what the data structure
      contains or is for).
    - (Style) Mixing spaces and tabs. Please always just use spaces.
    - (Perlism) Don't compare to 1 directly unless needed, just use boolean
      context
    - (Bug) Don't open the subtitle with mode of >> (append). Why would you
      want to append to subtitles?
    - (Bug) keys $json->{video}->{videoReferences} doesn't make sense: this is
      an array reference, not a hash reference. Since it's an array reference,
      just iterate over it directly, no need to bother with $i.
    - (Best practice) Don't use BAREWORD/global filehandle names. (They're
      global, difficult to pass as parameters or store in objects, and have
      other problems.)
    
    I tried testing this with http://www.svtplay.se/video/107384/flamingosjoar
    but get an error from rtmpdump. The server simply closes the connection:
    
      rtmpdump: ERROR: rtmp server requested close
    
    Maybe it requires a Swedish IP.

--- lib/FlashVideo/Site/Svtplay.pm
+++ lib/FlashVideo/Site/Svtplay.pm
@@ -1,7 +1,9 @@
 # Part of get-flash-videos. See get_flash_videos for copyright.
 package FlashVideo::Site::Svtplay;
+
 use strict;
 use warnings;
+
 use FlashVideo::Utils;
 use FlashVideo::JSON;
 
@@ -9,53 +11,61 @@ sub find_video {
   my ($self, $browser, $embed_url, $prefs) = @_;
   my @rtmpdump_commands;
     
-  if (!($browser->uri->as_string =~ m/video\/([0-9]*)/)) {
+  if ($browser->uri->as_string !~ m/video\/([0-9]*)/) {
     die "No video id found in url";
   }
 
-  my ($video_id) = $1;
+  my $video_id = $1;
   my $info_url = "http://www.svtplay.se/video/$video_id?output=json";
+
   $browser->get($info_url);
     
   if (!$browser->success) {
     die "Couldn't download $info_url: " . $browser->response->status_line;
   }
 
-  my $jsonstr = $browser->content;
-  my $json = from_json($jsonstr);
-  my $name = $json->{context}->{title};
-  my ($bitrate) = 0;
-  my $rtmp;
-  my $i;
-  foreach $i (keys $json->{video}->{videoReferences}) {
-    my ($rate) = int($json->{video}->{videoReferences}[$i]->{bitrate});
+  my $video_data = from_json($browser->content);
+  my $name = $video_data->{context}->{title};
+  my $bitrate = 0;
+  my $rtmp_url;
+
+  foreach my $video (@{ $video_data->{video}->{videoReferences} }) {
+    my $rate = int $video->{bitrate};
+
     if ($bitrate < $rate) {
-      $rtmp = $json->{video}->{videoReferences}[$i]->{url};
+      $rtmp_url = $video->{url};
       $bitrate = $rate;
     }
   }
 
-  if ($prefs->{subtitles} == 1) {
-    my $sub = $json->{video}->{subtitleReferences}[0]->{url}; 
-    if ($sub) {
-      info "Found subtitles: " . $sub;
-      $browser->get("$sub");
+  if ($prefs->{subtitles}) {
+    if (my $subtitles_url = $video_data->{video}->{subtitleReferences}[0]->{url}) {
+      info "Found subtitles URL: $subtitles_url";
+
+      $browser->get($subtitles_url);
+
+      if (!$browser->success) {
+        info "Couldn't download subtitles: " . $browser->status_line;
+      }
+
       my $srt_filename = title_to_filename($name, "srt"); 
-      my $srt_content = $browser->content;
-      open (SRT, '>>',$srt_filename) 
-	or die "Can't open subtitles file $srt_filename: $!";
-      binmode SRT, ':utf8';
-      print SRT $srt_content;
-      close SRT;
-    } else {
+
+      open my $srt_fh, '>', $srt_filename
+        or die "Can't open subtitles file $srt_filename: $!";
+      binmode $srt_fh, ':utf8';
+      print $srt_fh $browser->content;
+      close $srt_fh;
+    }
+    else {
       info "No subtitles found!";
     }
   }
+
   return {
-	  flv => title_to_filename($name, "flv"),
-	  rtmp => $rtmp,
-	  swfVfy => "http://www.svtplay.se/public/swf/video/svtplayer-2012.15.swf"
-	 };
+	  flv    => title_to_filename($name, "flv"),
+	  rtmp   => $rtmp_url,
+	  swfVfy => "http://www.svtplay.se/public/swf/video/svtplayer-2012.15.swf",
+  };
 }
 
 1;
