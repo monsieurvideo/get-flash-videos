75f56067ad57adb40db0365c25365017021abbf5
Nigel Taylor <njtaylor0101@gmail.com>
2012-11-02 13:42:52 +0000

    LWP::Protocol::https removed from libwww-perl afer 6.0 ensure combined includes LWP::Protocol::https

--- lib/FlashVideo/Mechanize.pm
+++ lib/FlashVideo/Mechanize.pm
@@ -1,6 +1,9 @@
 # Part of get-flash-videos. See get_flash_videos for copyright.
 package FlashVideo::Mechanize;
 use WWW::Mechanize;
+use LWP::Protocol::https;
+use Mozilla::CA;
+use IO::Socket::SSL;
 use FlashVideo::Downloader;
 use Encode ();
 
