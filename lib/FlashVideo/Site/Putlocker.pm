9fb7db80c1862454003e4f91e7f3fd4e878c6a8b
Zakariyya Mughal <zaki.mughal@gmail.com>
2012-12-05 14:42:12 -0600

    Putlocker: high quality format

--- lib/FlashVideo/Site/Putlocker.pm
+++ lib/FlashVideo/Site/Putlocker.pm
@@ -7,12 +7,16 @@ use HTML::Tree;
 use HTML::Entities qw(decode_entities);
 
 sub find_video {
-  my ($self, $browser, $embed_url) = @_;
+  my ($self, $browser, $embed_url, $prefs) = @_;
 
   die 'Could not retrieve video' unless ($browser->success);
 
+  my ($id) = ($embed_url =~ m,file/([^/]*),);
+  print $id,"\n";
+
   my ($filename) = title_to_filename(extract_title($browser));
   $filename =~ s/[\s\|_]*PutLocker[\s_]*//;
+  my $url; # the final URL
   
   #get the "hash" value from the HTML
   my $tree = HTML::Tree->new();
@@ -48,13 +52,34 @@ sub find_video {
   #parse the url and title out of the response - much easier to regex it out, as the XML has dodgy &'s.
   $browser->allow_redirects;
   my $contents = $browser->get($uri)->content;
+  # this is necessary to download both high quality and streaming version
   die 'Unable to download video information' unless ($browser->success);
-  my ($url) = ($contents =~ /url="(.*?)"/);
-  $url = decode_entities($url);
+  my ($stream_url) = ($contents =~ /url="(.*?)"/);
+  $stream_url = decode_entities($stream_url);
 
+  if($prefs->{quality} eq 'high') {
+    $url = get_high_quality($id, $streamID);
+  } else {
+    # get streaming version
+    $url = $stream_url;
+
+    if($url =~ /expired_link/) {
+      if( $page_html =~ m,"/(get_file\.php\?id=[^"]*)", ) {
+        # download original file if link available
+        my $download_page = $1;
+        $url = URI->new( "http://www.putlocker.com/$1" );
+        # this URL should be equivalent to what is returned by get_high_quality()
+      }
+    }
+  }
   info "Got the video URL: " . $url;
 
   return $url, $filename;
+}
+
+sub get_high_quality {
+  my ( $id, $key ) = @_;
+  return "http://www.putlocker.com/get_file.php?id=$id&key=$key&original=1";
 }
 
 1;
