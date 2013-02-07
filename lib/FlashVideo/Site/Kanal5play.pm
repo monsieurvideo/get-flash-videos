8d74100a8a64c094b55bc8ac88f1cf1b90321e51
Jonas <jonaskarlsson@fripost.org>
2013-02-07 19:47:53 +0100

    BUG: Kanal5play.pm did not iterate all video streams

--- lib/FlashVideo/Site/Kanal5play.pm
+++ lib/FlashVideo/Site/Kanal5play.pm
@@ -6,6 +6,8 @@ use warnings;
 use FlashVideo::Utils;
 use FlashVideo::JSON;
 
+our $VERSION = '0.01';
+sub Version() { $VERSION;}
 
 my $bitrates = {
   low    => 250000,
@@ -18,7 +20,7 @@ sub find_video {
   if (!($browser->uri->as_string =~ m/video\/([0-9]*)/)) {
     die "No video id found in url";
   }
-  my ($video_id) = $1;
+  my $video_id = $1;
   my $info_url = "http://www.kanal5play.se/api/getVideo?format=FLASH&videoId=$video_id";
   $browser->get($info_url);
 
@@ -26,19 +28,19 @@ sub find_video {
     die "Couldn't download $info_url: " . $browser->response->status_line;
   }
 
-  my $jsonstr = $browser->content;
-  my $json = from_json($jsonstr);
+  my $jsonstr  = $browser->content;
+  my $json     = from_json($jsonstr);
 
-  my $name = $json->{program}->{name};
-  my $episode = $json->{episodeNumber};
-  my $season = $json->{seasonNumber};
+  my $name     = $json->{program}->{name};
+  my $episode  = $json->{episodeNumber};
+  my $season   = $json->{seasonNumber};
   my $filename = sprintf "%s - S%02dE%02d", $name, $season, $episode;
-  my ($rtmp) = "rtmp://fl1.c00608.cdn.qbrick.com:1935/00608";
-  my ($playpath) = $json->{streams}[0]->{source};
+  my $rtmp     = "rtmp://fl1.c00608.cdn.qbrick.com:1935/00608";
+  my $playpath = $json->{streams}[0]->{source};
 
-  foreach my $stream ($json->{streams}[0]) {
-    my ($rate) = int($stream->{bitrate});
-    if ($bitrates->{$prefs->{quality}} eq $rate) {
+  foreach my $stream (@{$json->{streams}}) {
+    my $rate = int($stream->{bitrate});
+    if ($bitrates->{$prefs->{quality}} == $rate) {
       $playpath = $stream->{source};
       last;
     }
@@ -48,7 +50,7 @@ sub find_video {
     flv      => title_to_filename($filename, "flv"),
     rtmp     => $rtmp,
     playpath => $playpath,
-    swfVfy   => "http://www.kanal5play.se/flash/StandardPlayer.swf"
+    swfVfy   => "http://www.kanal5play.se/flash/K5StandardPlayer.swf"
   };
 }
 1;
