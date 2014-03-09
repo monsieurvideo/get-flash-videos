15f9c3503b8e1231d9295e982bc0aa37659fc189
Jonas Karlsson <jonaskarlsson@fripost.org>
2014-03-09 18:04:29 +0100

    Tv3play: New site changes

--- lib/FlashVideo/Site/Tv3play.pm
+++ lib/FlashVideo/Site/Tv3play.pm
@@ -1,9 +1,10 @@
 # Part of get-flash-videos. See get_flash_videos for copyright.
 package FlashVideo::Site::Tv3play;
 use strict;
+use warnings;
 use FlashVideo::Utils;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 sub Version() { $VERSION;}
 
 sub find_video {
@@ -17,65 +18,60 @@ sub find_video_viasat {
   info "Got video_id: $video_id";
   my $info_url = "http://viastream.viasat.tv/PlayProduct/$video_id";
   $browser->get($info_url);
-  my $variable = $browser->content;
-  $variable =~ s/\n//g;
-  my $title = ($variable =~ /<Title><!\[CDATA\[(.*?)\]\]><\/Title>/)[0];
+
+  my $xml = from_xml($browser->content);
+  my $product = $xml->{Product};
+  my $title = $product->{Title};
   my $flv_filename = title_to_filename($title, "flv");
 
-  # Subtitle Format not supported
-
-  # my $subtitle_url = ($variable =~ /<SamiFile>(.*)<\/SamiFile>/)[0];
-  # debug "Subtitle_url: $subtitle_url";
-  # if ($prefs->{subtitles} == 1) {
-  #   if (not $subtitle_url eq '') {
-  #     info "Found subtitles: $subtitle_url";
-  #     $browser->get("$subtitle_url");
-  #     my $srt_filename = title_to_filename($title, "srt"); 
-  #     convert_sami_subtitles_to_srt($browser->content, $srt_filename);
-  #   } else {
-  #     info "No subtitles found!";
-  #   }
-  # }
-
-  my @urls;
-  my $count = 0;
-  my $base = ($variable =~ /<Videos>(.*)<\/Videos>/)[0];
-  for ($count = 0; $count < 3; $count++){
-    my $video = ($base =~ /<Video>(.+)<\/Video>/p)[0];
-    if ($video eq ''){last;};
-    $base = ${^POSTMATCH};    
-    my $bitrate = ($video =~ /<BitRate>([0-9]*)<\/BitRate>/)[0];
-    my $url = ($video =~ /<Url><!\[CDATA\[(.*)]]><\/Url>/)[0];
-    if (not (($url =~ /http:\/\//)[0] eq '')){
-      $browser->get($url);
-      $variable = $browser->content;
-      $variable =~ s/\n//g;
-      $url = ($variable =~ /<Url>(.*)<\/Url>/)[0];
-    }
-    
-    $urls[$count++] = { 'bitrate' => $bitrate,
-		      'rtmp' => $url
-		    };
+  # Create array of video resolutions
+  my @videos;
+  if (ref $product->{Videos} eq 'HASH') {
+    push(@videos, $product->{Videos});
+  } else {
+    @videos = $product->{Videos};
   }
-  my $bitrate = 0;
-  my $rtmp;
-  my $new_bitrate;
 
+  # Collect the rtmp data for each resolution
+  my @urls;
+  foreach (@videos) {
+    my $video = $_->{Video};
+    my $bitrate = $video->{BitRate};
+    my $url = $video->{Url};
+
+    if ($url =~ m/http:\/\//){
+      $browser->get($url);
+      $xml = from_xml($browser->content);
+      $url = $xml->{'Url'};
+    }
+
+    my $rtmp_data = {
+      'swfVfy' => "http://flvplayer.viastream.viasat.tv/flvplayer/play/swf/MTGXPlayer-0.7.4.swf",
+      'flv'    => $flv_filename
+    };
+
+    if ($url =~ /(rtmp:.*)(mp4:.*)/) {
+      $rtmp_data->{'rtmp'} = $1;
+      $rtmp_data->{'playpath'} = $2;
+    } else {
+      $rtmp_data->{'rtmp'} = $url;
+    }
+
+    push(@urls, { 'bitrate' => $bitrate, 'rtmp_data' => $rtmp_data });
+  }
+
+  my $bitrate = 0;
+  my $new_bitrate;
+  my $rtmp_data;
   foreach (@urls) {
     $new_bitrate = int($_->{bitrate});
-    if($new_bitrate > $bitrate){
-        $bitrate = int($_->{bitrate});
-        $rtmp = $_->{rtmp};
+    if ($new_bitrate > $bitrate) {
+      $bitrate = int($_->{bitrate});
+      $rtmp_data = $_->{rtmp_data};
     }
   };
 
-  my $rnd = int(rand(10**9)) + 10**9;
-
-  return {
-	  rtmp => $rtmp,
-	  swfVfy => "http://flvplayer.viastream.viasat.tv/flvplayer/play/swf/player.swf?rnd=$rnd",
-	  flv => $flv_filename
-	 };
+  return $rtmp_data;
 }
 
 1;
