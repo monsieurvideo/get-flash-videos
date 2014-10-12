1aad8e20573ed7feb71ed0bf6d879eed3acc22b6
Spencer Krum <spencer.krum@hp.com>
2014-10-12 16:32:32 -0700

    Removing Youtu.be so it just works

--- lib/FlashVideo/Site/Youtu.pm
+++
@@ -1,7 +0,0 @@
-# Part of get-flash-videos. See get_flash_videos for copyright.
-package FlashVideo::Site::Youtu;
-
-# youtu.be is just an alias for youtube; everything else is handled further up
-use base 'FlashVideo::Site::Youtube';
-
-1;
