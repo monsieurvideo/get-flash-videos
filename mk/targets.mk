5aad7a6518a9d5e34af5643bb78bd6c37386d5cc
Nigel Taylor <njtaylor0101@gmail.com>
2015-10-23 12:57:46 +0100

    Add combine-tail and change to work with BSD make as well.

--- mk/targets.mk
+++ mk/targets.mk
@@ -1,8 +1,17 @@
-ifeq ($(findstring release,$(MAKECMDGOALS)),release)
-  export SCRIPT = $(BASEEXT)-$(VERSION)
-else
-  export SCRIPT = $(INST_SCRIPT)/$(BASEEXT)
-endif
+# removing these are not used.
+# t/url.t uses the SCRIPT env...
+# GNU make .....
+#ifeq ($(findstring release,$(MAKECMDGOALS)),release)
+#  export SCRIPT = $(BASEEXT)-$(VERSION)
+#else
+#  export SCRIPT = $(INST_SCRIPT)/$(BASEEXT)
+#endif
+# OpenBSD make
+#.if ${.TARGETS:C/.*(release).*/\1/} == "release"
+#	export SCRIPT=$(BASEEXT)-$(VERSION)
+#.else
+#	export SCRIPT=$(INST_SCRIPT)/$(BASEEXT)
+#.endif
 
 # Extra targets
 COMBINE = $(PERL) -I$(INST_LIB) utils/combine-perl.pl
@@ -21,17 +30,17 @@ $(BASEEXT)-$(VERSION): bin/$(BASEEXT) pm_to_blib .sitemodules \
 # This makes sure to 'use' all the Site modules, so that the combiner can pick
 # them all up.
 .sitemodules: lib/FlashVideo/Site/*.pm
-	ls $^ | sed -e 's!lib/!!' -e 's!/!::!g' -e 's/\.pm$$/ ();/' -e 's/^/use /' > $@
+	ls lib/FlashVideo/Site/*.pm| sed -e 's!lib/!!' -e 's!/!::!g' -e 's/\.pm$$/ ();/' -e 's/^/use /' > $@
 
 # Build a combined version which also includes our dependencies, this makes it
 # easier for people who cannot install Perl modules. (Note that it does still
 # need HTML::Parser, as this is XS, and optionally XML::Simple, but LWP and
 # Mechanize are included by this).
 
-COMBINED_SOURCES = utils/combine-head .sitemodules bin/$(BASEEXT)
+COMBINED_SOURCES = utils/combine-head .sitemodules bin/$(BASEEXT) utils/combine-tail
 
 combined-$(BASEEXT)-$(VERSION): combined-get_flash_videos
-	cp -p $^ $@
+	cp -p $? $@
 
 combined-$(BASEEXT): $(COMBINED_SOURCES)
 	$(COMBINE) --name="$@" $(COMBINED_SOURCES) > $@
@@ -41,4 +50,5 @@ clean:: extraclean
 
 extraclean:
 	rm -f $(EXTRATARGETS) .sitemodules mk/makemaker-wrap.mk MANIFEST MANIFEST.bak $(DISTVNAME).tar$(SUFFIX)
+
 
