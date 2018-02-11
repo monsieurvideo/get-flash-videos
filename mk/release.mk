5934c2251a92fb390caadba388a49448d9a348f2
Nigel Taylor <njtaylor0101@gmail.com>
2018-02-11 15:06:04 +0000

    Revised again

--- mk/release.mk
+++ mk/release.mk
@@ -7,9 +7,12 @@ release: release-test release-tag release-cpan release-upload deb
 	svn commit -m "Version $(VERSION)" wiki/Installation.wiki wiki/Version.wiki
 
 release-test: $(BASEEXT)-$(VERSION) release-combined test
-	@git status --porcelain | ( grep -q . &&\
-	  echo "Tree dirty, won't release."; exit 1); \
-          echo -n
+	@git status --porcelain | if grep -q . ; then \
+	   echo "Tree dirty, won't release."; \
+	   exit 1; \
+	 else \
+	   exit 0; \
+	 fi
 
 release-cpan: manifest metafile dist
 
